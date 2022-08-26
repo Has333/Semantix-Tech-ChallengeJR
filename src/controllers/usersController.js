b0629451d5eea71cc8e4ff06118d91569089c9cd
Has333 <williammds00@gmail.com>
2022-08-26 18:31:47 -0300

    feat: add api pagination

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,17 +7,20 @@ const apiURL = process.env.API_URL;
 class usersController {
   async listAll (req, res) {
     try {
+      const page = req.query.page   || 1;
+      const limit = req.query.limit || 10;
+
       const completeUsersModel = [];
-      const users = (await axios.get(`${apiURL}/users`)).data;
+      const users = (await axios.get(`${apiURL}/users?page=${page}&limit=${limit}`)).data;
 
       for (let user of users) {
         await sleep(1000);
         const addresses = (await axios.get(`${apiURL}/users/${user.id}/address`)).data;
         const contacts =  (await axios.get(`${apiURL}/users/${user.id}/contacts`)).data;
-
+        
         completeUsersModel.push(transformUserToDataModel(user, addresses, contacts));
       }
-      return res.status(200).json(FinalUsersModel);
+      return res.status(200).json(completeUsersModel);
 
     } catch (err) {
       return res.status(500).json({ message: err.message });
